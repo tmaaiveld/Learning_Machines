09ab84b441b360816e3b8a3bdffc5c5fc35f0cc6
PhPeKe <p.kersten@live.de>
2020-01-27 14:59:28 +0100

    new task, new branch

--- src/camera.py
+++ src/camera.py
@@ -4,7 +4,7 @@ import imutils
 
 class Camera:
 
-    def __init__(self, image, save_image=False):
+    def __init__(self, image, save_image=True):
         self.image = image
         self.save_image = save_image
 
@@ -84,6 +84,101 @@ class Camera:
 
         if self.save_image:
             path = "green_{}.png".format(step)
+            save = cv2.imwrite(path, self.image)
+            print('Image saved: {}'.format(save))
+
+        return x/x_ax, neg_x/x_ax, neg_y/y_ax
+
+    def capture_prey_image(self, sens_bound, step):
+        self.hsv = cv2.cvtColor(self.image, cv2.COLOR_BGR2HSV)
+
+        # define range of red color in HSV
+        # lower_green = np.array([36, 0, 0])
+        # upper_green = np.array([86, 255, 255])
+
+        #HSV?
+        # lower_red = np.array([110 - sens_bound, 70, 50])
+        # upper_red = np.array([70 + sens_bound, 255, 255])
+
+        # #BG3
+        # lower_red = np.array([47 - sens_bound, 15, 100])
+        # upper_red = np.array([20 + sens_bound, 56, 200])
+
+        lower_red1 = np.array([0, 100, 0])
+        upper_red1 = np.array([10, 255, 255])
+
+        lower_red2 = np.array([170, 100, 0])
+        upper_red2 = np.array([180, 255, 255])
+
+        # Threshold the RBG image to get only red colors
+        mask1 = cv2.inRange(self.hsv, lower_red1, upper_red1)
+        mask2 = cv2.inRange(self.hsv, lower_red2, upper_red2)
+
+        mask = cv2.bitwise_xor(mask1, mask2)
+        mask = cv2.erode(mask, None, iterations=2)
+        mask = cv2.dilate(mask, None, iterations=2)
+
+        ## slice the green
+        imask = mask > 0
+        img_red = np.zeros_like(self.image, np.uint8)
+        img_red[imask] = self.image[imask]
+
+        ## save
+        #cv2.imwrite("green.png", img_green)
+
+        # print(np.array(img_green))
+
+        # cv2.imshow('mask', mask)
+        # cv2.imshow('hsv', img_hsv)
+        # cv2.imshow('self', self.image)
+        # cv2.imshow('green', img_green)
+        # cv2.waitKey(0)
+
+        self.img_red = img_red
+
+        # find contours in the mask and initialize the current
+        # (x, y) center of the ball
+        self.contours = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+                                cv2.CHAIN_APPROX_SIMPLE)
+        self.contours = imutils.grab_contours(self.contours)
+
+        # lengths of axes of the image
+        x_ax = float(np.array(self.image).shape[1])
+        y_ax = float(np.array(self.image).shape[0])
+
+
+        x = 0  # bigger it is, more central it is, smaller, more off
+        y = None
+        neg_x = 0  #129  # bigger it is, more central it is, smaller, more off
+        neg_y = 640  # --> smaller, more desirable
+
+        if self.contours:
+            # x,y is top left coordinate of rectangle
+            # x and y are distances from object in positive axes directions
+
+            contours = []
+            for contour in self.contours:
+                x, y, w, h = cv2.boundingRect(contour)
+                contours.append([x,y,w,h])
+
+            contours = np.array(contours)
+            print("{} object{} detected".format(len(contours), 's' if len(contours) > 1 else ""))
+
+            x, y, w, h = contours[np.argmax(contours[:,2] * contours[:,3])]
+            # x, y, w, h = cv2.boundingRect(self.contours[0])
+            print("edges: {}, {}, {}, {}".format(x, y, w, h))
+
+            self.image = cv2.rectangle(self.image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            # distances from object in negative axes directions
+            neg_x = x_ax - x - w
+            neg_y = y_ax - y - h
+            print('distance: {}, {}, {}, {}'.format(x, y, neg_x, neg_y))
+            print('axis sum: {}, {}'.format(neg_x+x+w, neg_y+y+h))
+
+
+        if self.save_image:
+            #path = "green_{}.png".format(step)
+            path = "red.png"
             save = cv2.imwrite(path, self.image)
             print('Image saved: {}'.format(save))
 
